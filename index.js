a4aeb982be9a8360f0e02cf8647e83527ffe363b
Sheikh Jabed <jabedbd2295@gmail.com>
2024-12-01 23:40:09 +0600

    Basic Setup Done of Power Core Server Side

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Power Core Gym Server is Running");
+})
+
+app.listen(port, (req, res) => {
+    console.log(`Power Core Gym Server is Running on Port: ${port}`);
+})
