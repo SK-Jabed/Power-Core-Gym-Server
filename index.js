9cdbda02d982b5e40d86afcbbb0e22351e3bf5bc
Sheikh Jabed <jabedbd2295@gmail.com>
2025-01-30 22:35:43 +0600

    Current Port Updated for Power Core Gym Server

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 6000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
-
-
 
 app.get('/', (req, res) => {
     res.send("Power Core Gym Server is Running");
