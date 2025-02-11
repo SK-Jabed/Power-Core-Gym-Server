dc5e62da6754b699423471a44db1997b8dca0833
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-11 21:22:15 +0600

    Index JS File Updated

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
-
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 6000;
@@ -10,10 +9,10 @@ const port = process.env.PORT || 6000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Power Core Gym Server is Running");
-})
+app.get("/", (req, res) => {
+  res.send("Power Core Gym Server is Running");
+});
 
 app.listen(port, () => {
-    console.log(`Power Core Gym Server is Running on Port: ${port}`);
-})
+  console.log(`Power Core Gym Server is Running on Port: ${port}`);
+});
